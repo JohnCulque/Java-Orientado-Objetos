9330b6afef5b9fea26a0d896e8912cef15fa45da
Usuário Convidado <Guest@Pedros-MacBook-Pro.local>
2019-10-16 23:00:22 -0300

    exercicio 4

---
+++ exercicio 4/src/IfandElse.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class IfandElse {
+
+	public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in);
+			int codigo;
+			int quantidade;
+
+			System.out.println("Qual que é o codigo e a quantidade?");
+			codigo = sc.nextInt();
+			quantidade = sc.nextInt();
+			
+			double total;
+			if (codigo == 1) {
+				total = quantidade * 4.0;
+			}
+			else if (codigo == 2) {
+				total = quantidade * 4.5;
+			}
+			else if (codigo == 3) {
+				total = quantidade * 5.0;
+			}
+			else if (codigo == 4) {
+				total = quantidade * 2.0;
+			}
+			else {
+				total = quantidade * 1.5;
+			}
+
+			System.out.printf("Total: R$ %.2f%n", total);
+			sc.close();
+			
+
+	}
+
+}
